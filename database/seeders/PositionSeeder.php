e6493119e8d00fe7b07f709a4224ce789907ffa8
Serj <zavala180395@gmail.com>
2024-03-15 12:32:56 +0200

    Create Position model, migrate, seeder

---
+++ database/seeders/PositionSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\UserPosition;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class PositionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $positions = ['Developer', 'PM', 'Marketing', 'QA', 'HR', 'DevOps', 'Analyst'];
+
+        foreach ($positions as $position) {
+            UserPosition::create([
+                'title' => $position
+            ]);
+        }
+    }
+}

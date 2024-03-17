054baf8a6512495cb1f51412eaeba6e3aca789d1
Serj <zavala180395@gmail.com>
2024-03-17 16:35:29 +0200

    Added 404 response for routes

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Supports\ResponseSupport;
 use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
 use Illuminate\Http\Request;
@@ -35,6 +36,13 @@ class RouteServiceProvider extends ServiceProvider
 
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
+        });
+
+        Route::fallback(function () {
+            return ResponseSupport::error([
+                'success' => false,
+                'message' => 'Page not found.',
+            ], 404);
         });
     }
 }
